e9f7425265a44a4c907520807b6378dc9e26c294
Florent Guille <asgeltaren@gmail.com>
2022-10-12 10:52:41 +0200

    Added FlatDarkLaf

--- src/main/java/asgel/app/Main.java
+++ src/main/java/asgel/app/Main.java
@@ -1,8 +1,11 @@
 package asgel.app;
 
+import com.formdev.flatlaf.FlatDarkLaf;
+
 public class Main {
 
 	public static void main(String[] args) {
+		FlatDarkLaf.setup();
 		App app = new App();
 		app.start();
 	}
